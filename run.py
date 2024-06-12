3588b7aabaac93d385d4111136e3c7db99157053
Hugh Keenan <hugh.keenan114@gmail.com>
2024-06-12 19:23:46 +0000

    Add in constants to serve as basis for enemy positions and targets for the player to choose from

---
+++ run.py
@@ -0,0 +1,12 @@
+from random import randint
+
+TARGET_GUESSES = []
+for x in range(5):
+    TARGET_GUESSES.append(x)
+
+TARGET_COORDINATES = []
+for x in range(5):
+    TARGET_COORDINATES.append(x)
+
+print(TARGET_COORDINATES, TARGET_GUESSES)
+
